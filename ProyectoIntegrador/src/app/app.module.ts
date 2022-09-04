ef230194e3da2a1316341ba06da7a7eb5853a7bf
NataliMar <martineznatali@live.com.ar>
2022-09-03 23:24:24 -0300

    Se agrega banner

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgramaComponent } from './components/logo-arg-programa/logo-arg-programa.component';
+import { RedSocialComponent } from './components/red-social/red-social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgramaComponent,
+    RedSocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
